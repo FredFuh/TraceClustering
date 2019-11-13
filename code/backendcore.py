b373cbc93d2d30887ecd69fda2fdc0cd016c4cd6
Frederik Fuhrmann <frederik.fuhrmann@rwth-aachen.de>
2019-11-13 11:31:52 +0100

    check_sample_list function

--- code/backendcore.py
+++ code/backendcore.py
@@ -1,3 +1,4 @@
+import csv
 
 
 def check_sample_list(filepath):
@@ -8,5 +9,31 @@ def check_sample_list(filepath):
         input: csv file: id, cluster
              where cluster =1 if in cluster, else 0
 
-        output: dict:
-            cluster: contains list of ids for this cluster"""
+        output:
+        boolean: indicate if check was successful
+
+        dict: if successful
+            "cluster": contains list of ids for this cluster
+            "noncluster": contains list of the remaining ids
+        """
+    res = dict()
+    res['cluster'] = list()
+    res['noncluster'] = list()
+    boolvalue = True
+    with open(filepath) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        count = 0
+        for row in csv_reader:
+            if int(row[1]) == 1:
+                res['cluster'].append(row[0])
+            elif int(row[1]) == 0:
+                res['noncluster'].append(row[0])
+            else:
+                boolvalue = False
+            count += 1
+        if count < 20 or len(res['cluster']) == 0:
+            boolvalue = False
+    if boolvalue:
+        return boolvalue, res
+    else:
+        return boolvalue, None
--- code/tests.py
+++ code/tests.py
@@ -1,14 +1,15 @@
 import unittest
-
-
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+from backendcore import check_sample_list
 
 
 class SampleTest(unittest.TestCase):
     def test_check_sample_list(self):
-        self.assertEqual(True, True)
+        correct, _ = check_sample_list("test_data/correctsample.txt")
+        self.assertEqual(correct, True)
+        toosmall, _ = check_sample_list("test_data/toosmallsample.txt")
+        self.assertEqual(toosmall, False)
+        toomany, _ = check_sample_list("test_data/toomanyclusterssample.txt")
+        self.assertEqual(toomany, False)
 
 
 if __name__ == '__main__':
