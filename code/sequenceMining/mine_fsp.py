25780c635c08e50069c4386718bfc930644fbfaf
dslize <d.schleiz@web.de>
2019-11-16 18:16:53 +0100

    Change output format for sequence mining algorithm, since lists cannot function as keys in a dict

--- code/sequenceMining/mine_fsp.py
+++ code/sequenceMining/mine_fsp.py
@@ -1,13 +1,13 @@
 from pm4py.objects.log.importer.xes import factory as xes_importer
 from sequenceDB import *
-from mine_fsp_closed import *
+from mine_fsp_closed import mine_fsp_closed
 
 def mine_fsp(log_path, min_sup):
     ''' 
-    Mines the frequent sequent patterns (fsp's) of length 1,2 and the closed fsp's for the given event log.
-    The respective fsp's are organized in a dict, where the sequence ([Int]) is a key, and the corresponding value is its frequency.
+    Mines the frequent sequent patterns (fsp's) of length 1,2 and the closed fsp's for the given event log and minimum support.
+    The respective fsp's are organized in lists of tuples, where the first entry is the fsp and the second is its absolute support.
     
-    input: XES log file path
+    input: XES log file path, min_sup (absolute)
     
     output: (fsp_1, fsp_2, fsp_c)
     '''
@@ -15,11 +15,11 @@ def mine_fsp(log_path, min_sup):
     sdb = log_to_sdb(log_path)
 
     # As a byproduct, frequent subsequences of length 1 are computed during the computation of closed fsp's
-    fsp_1, fsp_c = mine_fsp_closed(sdb)
-    fsp_2 = mine_fsp_2(sdb)
+    fsp_1, fsp_c = mine_fsp_closed(sdb, min_sup)
+    fsp_2 = mine_fsp_2(sdb, min_sup)
 
     return fsp_1, fsp_2, fsp_c
 
 # placeholder
-def mine_fsp_2(sdb):
+def mine_fsp_2(sdb, min_sup):
     return dict()
--- code/sequenceMining/mine_fsp_closed.py
+++ code/sequenceMining/mine_fsp_closed.py
@@ -3,5 +3,34 @@ from sequenceDB import *
 # Mining closed frequent items sets using a slightly simplified version of the CloFAST algorithm, due to abusing the fact that our itemsets are always of cardinality 1.
 
 # placeholder
-def mine_fsp_closed(sdb):
-    return dict()
+def mine_fsp_closed(sdb, min_sup):
+    ''' 
+    Mines the frequent sequent patterns (fsp's) of length 1 and the closed fsp's for the given SequenceDB. A simplified version of the algorithm CloFAST is implemented,
+    with the major simplification being that itemsets of sequences from the SequenceDB always have exactly one element. Similar nomenclature to in the paper is used.
+    The respective output fsp's are organized in lists of tuples, where the first entry is the fsp and the second is its absolute support.
+    
+    Args: 
+        sdb (SequenceDB): The SequenceDB object
+        min_sup (int): The absolute minimum support of a frequent sequence pattern
+    
+    Returns:
+        [([int], int)], [([int], int)]: Returns two list of tuples, representing the fsp's of length 1 and the closed ones. A tuple's first entry is the fsp, while the second is the absolute support.
+    '''
+    # First build up the sparse id lists (sil) for all 1-itemsets, organised as [[Int]] where an activity index serves as the index to its id list in the sil
+    # In contrast to the paper, the sil's store indices of activities (0 indexed) and not positions which start at 1
+    # The CIET is not explicitly constructed, because the closed frequent itemsets are exactly the frequent 1-itemsets, since itemsets in our sequences are always of size 1
+    db = sdb.db
+    sils = build_sils(db, sdb.num_activities)
+            
+    return [], []
+
+def build_sils(db, num_activities):
+    '''
+    Constructs the sparse id lists for a given sequence database and the number of activities num_activities, assuming the activities in the database are 0,..,num_activities-1
+
+    Args:
+
+
+    '''
+
+    return []
