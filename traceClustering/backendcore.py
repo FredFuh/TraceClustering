a1e370c2681406bb9109f85810e40315c8fcc2d2
Frederik Fuhrmann <frederik.fuhrmann@rwth-aachen.de>
2019-11-18 15:14:49 +0100

    sampling method implemented

--- traceClustering/backendcore.py
+++ traceClustering/backendcore.py
@@ -1,44 +1,4 @@
-import csv
 from pm4py.objects.log.importer.xes import factory as xes_importer
-
-
-def check_sample_list(filepath):
-    """ checks if the given list fulfills criteria
-        - is big enoug (at least 20 ids)
-        - only one cluster and counterpart
-        - ids from cluster as well as from coutnerpart
-        input: csv file: id, cluster
-             where cluster =1 if in cluster, else 0
-
-        output:
-        boolean: indicate if check was successful
-
-        dict: if successful
-            "cluster": contains list of ids for this cluster
-            "noncluster": contains list of the remaining ids
-        """
-    res = dict()
-    res['cluster'] = list()
-    res['noncluster'] = list()
-    boolvalue = True
-    with open(filepath) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        count = 0
-        for row in csv_reader:
-            if int(row[1]) == 1:
-                res['cluster'].append(row[0])
-            elif int(row[1]) == 0:
-                res['noncluster'].append(row[0])
-            else:
-                boolvalue = False
-            count += 1
-        if count < 20 or len(res['cluster']) == 0:
-            boolvalue = False
-    if boolvalue:
-        return boolvalue, res
-    else:
-        return boolvalue, None
-
 
 def build_sequences(logfile):
     """
--- traceClustering/tests.py
+++ traceClustering/tests.py
@@ -1,16 +1,17 @@
 import unittest
-from backendcore import check_sample_list
+from sample_log.sample import check_sample_list, create_sample_log
 
 
 class SampleTest(unittest.TestCase):
     def test_check_sample_list(self):
-        correct, _ = check_sample_list("test_data/correctsample.txt")
+        correct, _ = check_sample_list("test_data/correctsample.csv")
         self.assertEqual(correct, True)
-        toosmall, _ = check_sample_list("test_data/toosmallsample.txt")
+        toosmall, _ = check_sample_list("test_data/toosmallsample.csv")
         self.assertEqual(toosmall, False)
-        toomany, _ = check_sample_list("test_data/toomanyclusterssample.txt")
+        toomany, _ = check_sample_list("test_data/toomanyclusterssample.csv")
         self.assertEqual(toomany, False)
-
+    def test_create_sample_log(self):
+        create_sample_log("test_data/test2.xes", "test_data/correctsample.csv", "test_data/samplefile.xes")
 
 if __name__ == '__main__':
     unittest.main()
