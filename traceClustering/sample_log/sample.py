9dbaaad342320b1b5f24cdbc6d291aeb113f5a98
Pandi,Ajay <ajay.pandi@ima-ifu.rwth-aachen.de>
2019-11-20 19:16:57 +0100

    Extending to multiple clusters

--- traceClustering/sample_log/sample.py
+++ traceClustering/sample_log/sample.py
@@ -7,6 +7,23 @@ from pm4py.objects.log.log import EventLog
 # Provide functions here for input event log and csv file path to return a pm4py EventLog object which only contains traces from the sample list
 # Potentially already implement to generate multiple sample sets if csv file contains information for multiple clusters
 
+def build_sequences(filepath):
+    """ 
+    Converts the input log into a dictionary with case ids as key and list of events as its values. 
+
+    Input: XES log filepath
+    
+    Output: Dictionary
+    """
+    sequence = {}
+    log = xes_importer.import_log(filepath)
+    
+    for trace in log:
+        sequence[trace.attributes['concept:name']] = [event['concept:name'] for event in trace]
+    
+    return sequence
+
+'''
 def check_sample_list(filepath):
     """ checks if the given list fulfills criteria
         - is big enoug (at least 20 ids)
@@ -43,7 +60,43 @@ def check_sample_list(filepath):
         return boolvalue, res
     else:
         return boolvalue, None
+'''
 
+def check_sample_list(filepath, seq):
+    """ 
+    Checks if all the given cases exist in the log and creates a dictionary object for each cluster
+        
+    Input: CSV file path, Sequences seq
+           CSV file - Format as below
+                file[][0] = case id 
+                file[][1] = cluster
+           Sequences - Dictionary output of build_sequences()
+               
+    Output: Result, Missing Cases
+            Result - Dictionary with different clusters as key, and its values are list of cases 
+            Missing cases - Case ids that do not exist in the log
+    """
+    result = dict()
+    complete_caseids = ""
+    missing_cases = []
+    
+    for key,value in seq.items():
+        complete_caseids = complete_caseids + " " + str(key)
+        
+    with open(filepath) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            if row[0] in complete_caseids:
+                if result.get(row[1]):
+                    result[row[1]].append(row[0])
+                else:
+                    result[row[1]] = [row[0]]
+            else:
+                missing_cases.append(row[0])
+
+    return result, missing_cases #use missing_cases in the calling code instead of boolvalue here.
+
+'''
 def create_sample_log(logpath, samplelist, goalpath):
     """ Stores the log sampled out of 'logpath' with the 'samplelist' at 'goalpath'
         uses 'check_sample_list' to validate samplelist"""
@@ -60,4 +113,23 @@ def create_sample_log(logpath, samplelist, goalpath):
             if trace.attributes['concept:name'] in sample['cluster']:
                 samplelog.append(trace)
         xes_exporter.export_log(samplelog, goalpath)
+'''
+def create_sample_logs(clus_dict, filepath):
+    """
+    Build separate logs with traces corresponding to each cluster
+    
+    Input: Dictionary output of check_sample_list(), XES log filepath
+    
+    Output: No return value
+    """
+    log = xes_importer.import_log(filepath)
 
+    for key,value in clus_dict.items():
+        args = {'attributes': log.attributes, 'extensions': log.extensions, 'omni_present': log.omni_present, 'classifiers': log.classifiers}
+        samplelog = TraceLog(**args)
+        goalpath = filepath[:-4] + "_" + key + ".xes"
+        for trace in log:
+            if trace.attributes['concept:name'] in value:
+                samplelog.append(trace)
+        
+        xes_exporter.export_log(samplelog, goalpath)
